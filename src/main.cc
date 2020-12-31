e3cba99efece5c06abf128a2c304e6aed9f7b83b
rmintz <raymondmintz11@gmail.com>
2020-12-31 13:29:02 -0800

    more skeleton

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <sqlite3.h>
 
 int main(){
+  sqlite3 *db;
+  int rc;
+
+  std::string db_path = "/tmp/kjv.db";
+  rc = sqlite3_open(db_path.c_str(), &db);
+
+  if(rc){
+    fprintf(stderr, "Cant open database: %s\n", sqlite3_errmsg(db));
+  }else{
+    printf("Opened database ok: %s\n", db_path.c_str());
+  }
+
+  sqlite3_close(db);
   printf("EOF\n");
 }
